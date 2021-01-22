0494e05ece96b2516007198db0b0e78ebe1e760f
Claire Yoon <claireyoon58@berkeley.edu>
2021-01-21 21:23:45 -0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,20 +4,23 @@
 public class Collatz {
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + "" + nextNumber(n));
+        while (n > 0) {
+            System.out.print(n + "");
+            n = nextNumber(n);
+        }
+
 
     }
 
     public static int nextNumber(int n) {
-        if (n == 1) {
-            return 0;
-        } else if (n % 2 == 0) {
-            int even = n / 2;
-            return (nextNumber(even) + 1);
-        } else {
-            int odd = (3 * n) + 1;
-            return (nextNumber(odd) + 1);
+            if (n % 2 == 0) {
+                int even = n / 2;
+                return even;
+            } else {
+                int odd = (3 * n) + 1;
+                return odd;
+            }
         }
-    }
+
 }
 

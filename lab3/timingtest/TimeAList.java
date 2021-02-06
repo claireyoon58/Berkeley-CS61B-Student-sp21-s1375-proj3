60cedb8c67d7579e012d8d48e26deb29f543144b
Claire Yoon <claireyoon58@berkeley.edu>
2021-02-05 21:58:21 -0800

    lab3

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -1,11 +1,15 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
 
 /**
  * Created by hug.
  */
 public class TimeAList {
-    private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+    private static void printTimingTable(ArrayList<Integer> Ns, ArrayList<Double> times, ArrayList<Integer> opCounts) {
         System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
         System.out.printf("------------------------------------------------------------\n");
         for (int i = 0; i < Ns.size(); i += 1) {
@@ -22,6 +26,17 @@ public class TimeAList {
     }
 
     public static void timeAListConstruction() {
-        // TODO: YOUR CODE HERE
+        ArrayList<Double> t = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000));
+
+        for (int i = 0; i < numbers.size(); i++) {
+            AList<Integer> timeconstruct = new AList<Integer>();
+            Stopwatch watch = new Stopwatch();
+            for (int b = 0; b < numbers.get(i); b++) {
+                timeconstruct.addLast((1));
+            }
+            t.add(watch.elapsedTime());
+        }
+        printTimingTable(numbers, t, numbers);
     }
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,11 +1,14 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 /**
  * Created by hug.
  */
 public class TimeSLList {
-    private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+    private static void printTimingTable(ArrayList<Integer> Ns, ArrayList<Double> times, ArrayList<Integer> opCounts) {
         System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
         System.out.printf("------------------------------------------------------------\n");
         for (int i = 0; i < Ns.size(); i += 1) {
@@ -22,7 +25,22 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        ArrayList<Double> t = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000));
+        ArrayList<Integer> Ops = new ArrayList<>(Arrays.asList(10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000));
+
+        for (int i = 0; i < numbers.size(); i++) {
+            SLList<Integer> run = new SLList<>();
+            for (int b = 0; b < numbers.get(i); b++) {
+                run.addLast(1);
+            }
+            Stopwatch watch = new Stopwatch();
+            for (int c = 0; c < 10000; c++) {
+                run.getLast();
+            }
+            t.add(watch.elapsedTime());
+        }
+        printTimingTable(numbers, t, Ops);
     }
 
 }

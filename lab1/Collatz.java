f732b524e7560855ddb7717ec5de39aae237f2c5
Claire Yoon <claireyoon58@berkeley.edu>
2021-02-17 00:40:14 -0800

    Finished Lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -10,19 +10,20 @@ public class Collatz {
         if (n  == 128) {
             return 1;
         } else if (n % 2 == 0) {
-            return n*2;
+            return n * 2;
         } else {
-            return 3*n +1;
+            return 3*n + 1;
         }
     }
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + "" + nextNumber(n));
-        while (n > 0) {
-            System.out.print(n + "");
+        while (n > 1) {
+            System.out.print(n + " ");
             n = nextNumber(n);
         }
+        System.out.print(n + " ");
+        System.out.println();
     }
 
 

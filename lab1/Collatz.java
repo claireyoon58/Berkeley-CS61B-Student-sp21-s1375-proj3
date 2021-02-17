577f4123a54977d47b18fae1c5d1bed890499373
Claire Yoon <claireyoon58@berkeley.edu>
2021-02-17 00:41:45 -0800

    Finished Lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -7,13 +7,15 @@ public class Collatz {
 
 
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n % 2 == 0) {
-            return n * 2;
-        } else {
-            return 3*n + 1;
+        if (n % 2 == 0)
+        {
+            n = n / 2;
         }
+        else {
+            n = (3 * n) + 1;
+        }
+        return n;
+
     }
 
     public static void main(String[] args) {

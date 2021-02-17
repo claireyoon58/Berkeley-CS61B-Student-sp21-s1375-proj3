9262ecd82b042da321c88017d1616bde4d6ca596
Claire Yoon <claireyoon58@berkeley.edu>
2021-02-16 23:33:08 -0800

    ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -16,69 +16,99 @@ public class TestArrayDequeEC {
 
 //        List methods = new ArrayList();
 
-        for (int i = 0; i < 10000; i ++) {
-            int randomNum = StdRandom.uniform(100);
-            a.addFirst(randomNum);
-            b.addFirst(randomNum);
-        }
         for (int i = 0; i < 10000; i++) {
-            int expected = a.get(i);
-            int actual = b.get(i);
-            assertEquals(expected, actual);
-            System.out.println("addFirst(" + i + ")");
+            int methodcall = StdRandom.uniform(0, 4);
+            int randomNum = StdRandom.uniform(1000);
+
+            if (methodcall == 0) {
+                a.addFirst(randomNum);
+                b.addFirst(randomNum);
+                int expected = a.get(0);
+                int actual = b.get(0);
+                assertEquals(expected, actual);
+                System.out.println("addFirst(" + i + ")");
+            } else if (methodcall == 1) {
+                a.addLast(randomNum);
+                b.addLast(randomNum);
+                int expected = a.get(a.size() - 1);
+                int actual = b.get(b.size() - 1);
+                assertEquals(expected, actual);
+                System.out.println("addLast(" + i + ")");
+            } else if (methodcall == 2) {
+                if (a.size() != 0 && b.size() != 0) {
+                    int expected = a.removeFirst();
+                    int actual = b.removeFirst();
+                    assertEquals(expected, actual);
+                    System.out.println("removeFirst()");
+                }
+            } else if (methodcall == 3) {
+                if (a.size() != 0 && b.size() != 0) {
+                    int expected = a.removeLast();
+                    int actual = b.removeLast();
+                    assertEquals(expected, actual);
+                    System.out.println("removeLast");
+                }
+            }
         }
+    }
+}
+
+
+
+//        for (int i = 0; i < 10000; i++) {
+//            int expected = a.get(i);
+//            int actual = b.get(i);
+//            assertEquals(expected, actual);
+//            System.out.println("addFirst(" + i + ")");
+//        }
 //        addlast
 
 
-        for (int i = 0; i < 10000; i ++) {
-            if (a.size() != 0 && b.size() != 0) {
-                int randomNum = StdRandom.uniform(100);
-                a.addLast(randomNum);
-                b.addLast(randomNum);
+//        for (int i = 0; i < 10000; i ++) {
+//            if (a.size() != 0 && b.size() != 0) {
+//                int randomNum = StdRandom.uniform(100);
+//                a.addLast(randomNum);
+//                b.addLast(randomNum);
 //                ethods.add("addLast(" + randomNum + ")")m;
             }
         }
 
-        for (int i = 0; i < 10000; i++) {
-            int expected = a.get(i);
-            int actual = b.get(i);
-            assertEquals(expected, actual);
-            System.out.println("addLast(" + i + ")");
-        }
+//        for (int i = 0; i < 10000; i++) {
+//            int expected = a.get(i);
+//            int actual = b.get(i);
+//            assertEquals(expected, actual);
+//            System.out.println("addLast(" + i + ")");
+//        }
 
 //        removeFirst
 
-        for (int i = 0; i < 10000; i ++) {
-            if (a.size() != 0 && b.size() != 0) {
-                int randomNum = StdRandom.uniform(100);
-                a.removeFirst();
-                b.removeFirst();
+//        for (int i = 0; i < 10000; i ++) {
+//            if (a.size() != 0 && b.size() != 0) {
+//                int randomNum = StdRandom.uniform(100);
+//                a.removeFirst();
+//                b.removeFirst();
 //                methods.add("removeFirst()");
             }
         }
-        for (int i = 0; i < 10000; i++) {
-            int expected = a.get(i);
-            int actual = b.get(i);
-            assertEquals(expected, actual);
-            System.out.println("removeFirst(" + i + ")");
-        }
+//        for (int i = 0; i < 10000; i++) {
+//            int expected = a.get(i);
+//            int actual = b.get(i);
+//            assertEquals(expected, actual);
+//            System.out.println("removeFirst(" + i + ")");
+//        }
 
 
 //        removeLast
 
-        for (int i = 0; i < 10000; i ++) {
-            if (a.size() != 0 && b.size() != 0) {
-                int randomNum = StdRandom.uniform(100);
-                a.removeLast();
-                b.removeLast();
-            }
+//        for (int i = 0; i < 10000; i ++) {
+//            if (a.size() != 0 && b.size() != 0) {
+//                int randomNum = StdRandom.uniform(100);
+//                a.removeLast();
+//                b.removeLast();
+//            }
         }
-        for (int i = 0; i < 10000; i++) {
-            int expected = a.get(i);
-            int actual = b.get(i);
-            assertEquals(expected, actual );
-            System.out.println("removeLast(" + i + ")");
-        }
-
-    }
-}
+//        for (int i = 0; i < 10000; i++) {
+//            int expected = a.get(i);
+//            int actual = b.get(i);
+//            assertEquals(expected, actual );
+//            System.out.println("removeLast(" + i + ")");

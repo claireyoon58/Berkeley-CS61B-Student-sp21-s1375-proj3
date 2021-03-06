d528f633c56c9791499dbe1bf98d22bc0d0a5ed5
Claire Yoon <claireyoon58@berkeley.edu>
2021-03-05 23:22:23 -0800

    lab7

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -16,12 +16,13 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         private Node(K key, V val) {
             this.key = key;
             this.val = val;
-            this.size = 1;
         }
     }
 
 
-
+    public void printInOrder() {
+        System.out.print(this);
+    }
 
     @Override
     public void clear(){
@@ -31,7 +32,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     @Override
     public boolean containsKey(K key){
-        return get(key)!=null;
+        return get(key) != null;
     }
 
     @Override
@@ -40,54 +41,56 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     }
 
     private V get(Node n, K key){
-        if(n==null){
+        if (n == null) {
             return null;
         }
-        int cmp=key.compareTo(n.key);
-        if(cmp==0){
+        int cmp = key.compareTo(n.key);
+        if (cmp == 0){
             return n.val;
         }
-        else if(cmp<0){
+        else if (cmp < 0) {
             return get(n.left, key);
         }
-        // key may be in the right
-        else{
+        else if (cmp > 0) {
             return get(n.right, key);
         }
+        return null;
     }
 
     @Override
-    public int size(){
+    public int size() {
         return size;
     }
 
     @Override
-    public void put(K key, V value){
+    public void put(K key, V value) {
         root = insert(root, key, value);
     }
 
-    private Node insert(Node n, K key, V value){
-        if(n == null){
-            return new Node(key, value);
+    private Node insert(Node n, K key, V value) {
+        if(n == null) {
+            n = new Node(key, value);
+            size += 1;
         }
 
         int cmp = key.compareTo(n.key);
-        if(cmp == 0){
+        if (cmp == 0) {
             n.val = value;
         }
-        else if(cmp < 0){
-            n.left=insert(n.left, key, value);
+        else if (cmp < 0) {
+            n.left = insert(n.left, key, value);
         }
-        else{
+        else {
             n.right = insert(n.right,key,value);
         }
-        n.size = size(n.right) + size(n.left)+1;
         return n;
     }
 
     private int size(Node n){
-        if(n == null) return 0;
-        return n.size;
+        if(n == null) {
+            return 0;
+        }
+        return size;
     }
 
     public BSTMap() {

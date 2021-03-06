363aa1e2462ca62390d5ecf2eacdd009e66f2b77
Claire Yoon <claireyoon58@berkeley.edu>
2021-03-05 22:56:26 -0800

    lab7

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,116 @@
+package bstmap;
+import java.util.Set;
+import java.util.Iterator;
+
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    private Node root;
+    private int size;
+
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+        private int size;
+
+        private Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+            this.size = 1;
+        }
+    }
+
+
+
+
+    @Override
+    public void clear(){
+        root = null;
+        size = 0;
+    }
+
+    @Override
+    public boolean containsKey(K key){
+        return get(key)!=null;
+    }
+
+    @Override
+    public V get(K key){
+        return get(root, key);
+    }
+
+    private V get(Node n, K key){
+        if(n==null){
+            return null;
+        }
+        int cmp=key.compareTo(n.key);
+        if(cmp==0){
+            return n.val;
+        }
+        else if(cmp<0){
+            return get(n.left, key);
+        }
+        // key may be in the right
+        else{
+            return get(n.right, key);
+        }
+    }
+
+    @Override
+    public int size(){
+        return size;
+    }
+
+    @Override
+    public void put(K key, V value){
+        root = insert(root, key, value);
+    }
+
+    private Node insert(Node n, K key, V value){
+        if(n == null){
+            return new Node(key, value);
+        }
+
+        int cmp = key.compareTo(n.key);
+        if(cmp == 0){
+            n.val = value;
+        }
+        else if(cmp < 0){
+            n.left=insert(n.left, key, value);
+        }
+        else{
+            n.right = insert(n.right,key,value);
+        }
+        n.size = size(n.right) + size(n.left)+1;
+        return n;
+    }
+
+    private int size(Node n){
+        if(n == null) return 0;
+        return n.size;
+    }
+
+    public BSTMap() {
+        root = null;
+        size = 0;
+    }
+
+
+
+
+
+    public  V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+    public  V remove(K key, V value){
+        throw new UnsupportedOperationException();
+    }
+    public  Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+}
